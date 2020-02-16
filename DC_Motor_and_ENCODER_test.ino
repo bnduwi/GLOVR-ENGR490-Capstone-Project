f70f656c650645e2981cac5d1e2282f135cf76ae
Chris <34405692+tripol6@users.noreply.github.com>
2020-02-15 19:45:41 -0500

    Add files via upload

---
+++ DC_Motor_and_ENCODER_test.ino
@@ -0,0 +1,55 @@
+#include <Encoder.h>
+
+// Change these pin numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+#define MOTOR_IN1 6
+#define MOTOR_IN2 7
+Encoder knobLeft(11, 12);
+//Encoder knobRight(7, 8);
+
+
+void setup() {
+  pinMode(MOTOR_IN1, OUTPUT);
+  pinMode(MOTOR_IN2, OUTPUT);
+//   avoid using pins with LEDs attached
+  Serial.begin(9600);
+  Serial.println("Encoder and Motor Test:");
+
+
+}
+
+long positionLeft  = -999;
+//long positionRight = -999;
+
+void loop() {
+
+  // ramp up forward
+digitalWrite(MOTOR_IN1, LOW);
+
+analogWrite(MOTOR_IN2, 255);
+delay(10);
+
+  long newLeft, newRight;
+  newLeft = knobLeft.read();
+  //newRight = knobRight.read();
+ // if (newLeft != positionLeft || newRight != positionRight) {
+    if (newLeft != positionLeft) {
+    Serial.print("Left = ");
+    Serial.print(newLeft);
+    //Serial.print(", Right = ");
+    //Serial.print(newRight);
+    //Serial.println();
+    positionLeft = newLeft;
+    //positionRight = newRight;
+  }
+  // if a character is sent from the serial monitor,
+  // reset both back to zero.
+  if (Serial.available()) {
+    Serial.read();
+    Serial.println("Reset value to zero");
+    knobLeft.write(0);
+    //knobRight.write(0);
+  }
+}
